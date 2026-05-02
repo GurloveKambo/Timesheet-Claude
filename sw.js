5dc5ef3c2b35b0ee06bb4a4e6e95dd518dbbd19e
GurloveKambo <integrityneeds@gmail.com>
2026-05-02 16:53:07 +1000

    Update sw.js

--- sw.js
+++ sw.js
@@ -14,7 +14,7 @@
    ===================================================== */
 
 // ⬇️ CHANGE THIS every time you deploy an update
-const CACHE_VERSION = 'v2.8';
+const CACHE_VERSION = 'v2.9';
 const CACHE_NAME    = 'fieldsheet-' + CACHE_VERSION;
 
 // Every file the app needs to work offline.
